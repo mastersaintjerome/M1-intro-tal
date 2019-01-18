5435d8fe7aaf1b00a5f66e7fe5ebe01746d836c7
Gaetan <barbaria.gaetan@gmail.com>
2019-01-18 03:34:19 +0100

    modif searchWordCode et ajout du compteur

---
+++ src/Compteur.java
@@ -0,0 +1,59 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Compteur {
+
+	private Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	private Scanner sc = new Scanner(System.in);
+	
+	public boolean hasNextLine() {
+		return sc.hasNextLine();
+	}
+	
+	public void readLine() {
+		String line = sc.nextLine();
+		if( ! line.isEmpty()) {
+			String[] parsedLine = line.split(" ");
+			
+			for(String code : parsedLine) {
+				
+				int key = Integer.parseInt(code);
+				
+				if( map.containsKey(key) ) {
+					map.put(key, map.get(key) + 1);
+				}else {
+					map.put(key, 1);
+				}
+			}
+		}
+	}
+	
+	public void sortMap() {
+		Map<Integer, Integer> treeMap = new TreeMap<>(
+                (Comparator<Integer>) (o1, o2) -> o1.compareTo(o2)
+        );
+		treeMap.putAll(map);
+		map = treeMap;
+	}
+	
+	public void printMap() {
+		System.out.println("La map contient les éléments suivants: ");
+		for(int key : map.keySet()) {
+			System.out.println("    " + key + " " + map.get(key));
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Compteur comp = new Compteur();
+		
+		comp.readLine();
+		comp.sortMap();
+		comp.printMap();
+		
+	}
+	
+}
--- src/Tokenize.java
+++ src/Tokenize.java
@@ -93,28 +93,66 @@ public class Tokenize {
 	
 	public int searchWordCode(String word) {
 		current = root;
-		char c;
+		char c = 0;
+		boolean needNewChar = true;
+		boolean isFind = false;
 		int code = -1;
-		while(word.length() > 0) {
-			c = word.charAt(0);
+
+		while(! isFind) {
+			if(needNewChar) {
+				c = word.charAt(0);
+				needNewChar = false;
+			}
 			if(current != null) {
 				code = current.getCode();
 				if(this.current.getChar() == c){
 					current = this.current.getFilsDroit();
+					needNewChar = true;
 				}else{
 					current = this.current.getFilsGauche();
 				}
 				word = word.substring(1);
+			}
+			
+			if(word.length() <= 0) {
+				isFind = true;
 			}
 		}
 		current = root;
 		return code;
 	}
 	
-	public String tokenizeFile(String FilePath) {
-		rd = new Reader(FilePath);
-
-		return null;
-	}
+    public String tokenizeStr(String str) {        
+        StringBuilder strTokenized = new StringBuilder(50);
+        String[] parts = str.split(" ");
+        int partsLength = parts.length;
+        int temp;
+        int i = 0;
+        while (i < partsLength) { 
+            temp = searchWordCode(parts[i]);
+            if(temp > 0){
+                int j = i;
+                String tempStr = parts[j];
+                int tempLast = temp;
+                while((temp != 0) && (j < partsLength)){
+                    temp = tempLast;
+                    j++;
+                    tempStr += "_" + parts[j];
+                    tempLast = searchWordCode(tempStr);
+                }
+                strTokenized.append(temp);
+                strTokenized.append(" ");
+                i++;
+            }else{
+                do{
+                    i++;
+                    temp = searchWordCode(parts[i]);
+                }while((temp == 0) && (i < partsLength));
+                strTokenized.append(0);
+                strTokenized.append(" ");
+            }
+        }
+        return strTokenized.toString();
+    }
 
 }
