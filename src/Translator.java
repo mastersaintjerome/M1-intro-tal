d1a463bb3fc0872c90080bb1219ea31e59b18c23
moonlight83340 <gaetan.perrot@etu.univ-amu.fr>
2019-02-05 14:13:10 +0100

    start translator

---
+++ src/Translator.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gaëtan
+ */
+public class Translator {
+    private final List<Triplet<Integer, Integer, Double>> translatorTable;
+    
+    public Translator(){
+        translatorTable = new ArrayList<>();
+    }
+}
